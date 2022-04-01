f1dd16b165fb79911ca676b9c91a36c02dd0907a
rahim iqbal <superiqu@gmail.com>
2022-04-01 17:16:43 +0600

    add object literal annotaitons

--- FEATURES/annotations/variables.ts
+++ FEATURES/annotations/variables.ts
@@ -9,3 +9,22 @@ let nothing: undefined = undefined;
 
 //built in objects
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumber: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, false];
+
+//classes
+class Car {
+
+}
+
+const car: Car = new Car();
+
+//Object literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 5
+  //a: 20 //--type error--//
+}
