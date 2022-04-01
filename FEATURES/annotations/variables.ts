9d7bc30b64d1ac83535c0d74115c6380268d1156
rahim iqbal <superiqu@gmail.com>
2022-04-01 17:01:45 +0600

    add annotaions with variables

---
+++ FEATURES/annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = false;
+
+// apples = 'apple' //type error
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
