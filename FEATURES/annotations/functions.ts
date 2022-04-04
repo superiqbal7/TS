358fdf49234b74951965cc261ddcb4f071d737e2
rahim iqbal <superiqu@gmail.com>
2022-04-04 14:24:23 +0600

    add void and never annotation for functions

--- FEATURES/annotations/functions.ts
+++ FEATURES/annotations/functions.ts
@@ -5,3 +5,21 @@ const add = (a: number, b: number): number => {
 const subtract = (a: number, b: number) => { //return type inference
   return a - b;
 }
+
+//Void & Never
+
+//Return type void when we don't return any value
+
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+//Return type never when we don't execute the function completely'
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+}
+
+const throwError2 = (message: string): void => {
+  if(!message) throw new Error(message);
+}
