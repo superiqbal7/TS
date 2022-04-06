aa6a4dee4efd8c7ae63664ecb014fa49270af50a
rahim iqbal <superiqu@gmail.com>
2022-04-07 03:14:06 +0600

    separate swapping and comparison

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,31 +1,9 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                //All of this only works if collection is number[]
-                //If collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                //ONly going to work if collection is a string
-                //If collection is a string, do this logic instead:
-                //--logic to compare and swap characters in string
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    }
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
 const array = [2, 4, 1, 5, 3];
-const sorter = new Sorter(array);
+const numbersCollection = new NumbersCollection_1.NumbersCollection(array);
+const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,33 +1,9 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
+const array = [2, 4, 1, 5, 3];
 
-    for(let i = 0; i < length; i++) {
-      for(let j = 0; j < length-i-1; j++) {
-
-        //All of this only works if collection is number[]
-        //If collection is an array of numbers
-        if(this.collection instanceof Array) {
-          if(this.collection[j] > this.collection[j + 1]){
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        //ONly going to work if collection is a string
-        //If collection is a string, do this logic instead:
-        //--logic to compare and swap characters in string
-        if(typeof this.collection === 'string'){
-
-        }
-      }
-    }
-  }
-}
-const array = [2, 4, 1, 5, 3]
-const sorter = new Sorter(array);
+const numbersCollection = new NumbersCollection(array);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection)
